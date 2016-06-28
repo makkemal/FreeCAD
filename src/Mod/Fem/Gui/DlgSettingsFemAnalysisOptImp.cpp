f782816a00ce1c1042fa1a44982a0946cd5188fd
vdwalts <vdwalts>
2016-06-28 14:29:25 +0200

    FEM: Add fem preferences to the Freecad preferences menu

--- src/Mod/Fem/Gui/DlgSettingsFemAnalysisOptImp.cpp
+++ src/Mod/Fem/Gui/DlgSettingsFemAnalysisOptImp.cpp
@@ -26,80 +26,69 @@
 #include "PreCompiled.h"
 
 #include "Gui/Application.h"
-#include "DlgSettingsFemImp.h"
+#include "DlgSettingsFemAnalysisOptImp.h"
 #include <Gui/PrefWidgets.h>
 
 using namespace FemGui;
 
-DlgSettingsFemImp::DlgSettingsFemImp( QWidget* parent )
+DlgSettingsFemAnalysisOptImp::DlgSettingsFemAnalysisOptImp( QWidget* parent )
   : PreferencePage( parent )
 {
     this->setupUi(this);
 }
 
-DlgSettingsFemImp::~DlgSettingsFemImp()
+DlgSettingsFemAnalysisOptImp::~DlgSettingsFemAnalysisOptImp()
 {
     // no need to delete child widgets, Qt does it all for us
 }
 
-void DlgSettingsFemImp::saveSettings()
+void DlgSettingsFemAnalysisOptImp::saveSettings()
 {
     ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath
-        ("User parameter:BaseApp/Preferences/Mod/Fem");
-    hGrp->SetInt("AnalysisType", cb_analysis_type->currentIndex());
-
-    fc_analysis_working_directory->onSave();
-    cb_int_editor->onSave();
-    fc_ext_editor->onSave();
-    fc_ccx_binary->onSave();
-    fc_z88_binary->onSave();
-    cb_analysis_type->onSave();
-    sb_eigenmode_number->onSave();
-    dsb_eigenmode_high_limit->onSave();
-    dsb_eigenmode_low_limit->onSave();
-    cb_use_built_in_materials->onSave();
-    cb_use_mat_from_config_dir->onSave();
-    cb_use_mat_from_custom_dir->onSave();
-    fc_custom_mat_dir->onSave();
-    cb_restore_result_dialog->onSave();
+        ("User parameter:BaseApp/Preferences/Mod/Fem/AnalysisOpt");
+    hGrp->SetInt("Solver", cmb_solver->currentIndex());
+        
+    //OvG: Solver settings
+    sb_ccx_numcpu->onSave();         //Number of CPUs
+    cmb_solver->onSave();
+    cb_ccx_non_lin_geom->onSave();
+    cb_hide_constraint->onSave();
+    
+    cb_static->onSave();
+    sb_ccx_max_iterations->onSave(); //Max number of iterations
+    dsb_ccx_initial_time_step->onSave(); //Initial time step
+    dsb_ccx_analysis_time->onSave(); //Analysis time
 }
 
-void DlgSettingsFemImp::loadSettings()
+void DlgSettingsFemAnalysisOptImp::loadSettings()
 {
-    fc_analysis_working_directory->onRestore();
-    cb_int_editor->onRestore();
-    fc_ext_editor->onRestore();
-    fc_ccx_binary->onRestore();
-    fc_z88_binary->onRestore();
-    cb_analysis_type->onRestore();
-    sb_eigenmode_number->onRestore();
-    dsb_eigenmode_high_limit->onRestore();
-    dsb_eigenmode_low_limit->onRestore();
-    cb_use_built_in_materials->onRestore();
-    cb_use_mat_from_config_dir->onRestore();
-    cb_use_mat_from_custom_dir->onRestore();
-    fc_custom_mat_dir->onRestore();
-    cb_restore_result_dialog->onRestore();
+    //OvG: Solver settings
+    sb_ccx_numcpu->onRestore();         //Number of CPUs
+    cmb_solver->onRestore();
+    cb_ccx_non_lin_geom->onRestore();
+    cb_hide_constraint->onRestore();
+    
+    cb_static->onRestore();
+    sb_ccx_max_iterations->onRestore(); //Max number of iterations
+    dsb_ccx_initial_time_step->onRestore(); //Initial time step
+    dsb_ccx_analysis_time->onRestore(); //Analysis time
 
     ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath
-        ("User parameter:BaseApp/Preferences/Mod/Fem");
-    int index =  hGrp->GetInt("AnalysisType", 0);
-    if (index > -1) cb_analysis_type->setCurrentIndex(index);
+        ("User parameter:BaseApp/Preferences/Mod/Fem/AnalysisOpt");
+        int index =  hGrp->GetInt("Solver", 0);
+    if (index > -1) cmb_solver->setCurrentIndex(index);
 }
 
 /**
  * Sets the strings of the subwidgets using the current language.
  */
-void DlgSettingsFemImp::changeEvent(QEvent *e)
+void DlgSettingsFemAnalysisOptImp::changeEvent(QEvent *e)
 {
     if (e->type() == QEvent::LanguageChange) {
-        int c_index = cb_analysis_type->currentIndex();
-        retranslateUi(this);
-        cb_analysis_type->setCurrentIndex(c_index);
     }
     else {
         QWidget::changeEvent(e);
     }
 }
 
-#include "moc_DlgSettingsFemImp.cpp"
+#include "moc_DlgSettingsFemAnalysisOptImp.cpp"
