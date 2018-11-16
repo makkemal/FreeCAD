dc02b0eebdb286805696d7a506228a97b378d847
makkemal <makkemal@gmail.com>
2018-11-16 07:14:44 +0200

    FEM: Update GPL

--- src/Mod/Fem/femguiobjects/_ViewProviderFemConstraintAutoContact.py
+++ src/Mod/Fem/femguiobjects/_ViewProviderFemConstraintAutoContact.py
@@ -1,6 +1,6 @@
 # ***************************************************************************
 # *                                                                         *
-# *   Copyright (c) 2016 - Michael Hindley                                  *
+# *   Copyright (c) 2016 - FreeCAD                                 *
 # *                                                                         *
 # *   This program is free software; you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License (LGPL)    *
